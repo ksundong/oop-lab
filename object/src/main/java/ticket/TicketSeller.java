342b4c4a83917038859b9139c5cb2e59b111479c
Dion <ksundong@gmail.com>
2021-05-08 23:27:26 +0900

    feat: 판매원을 구현한 TicketSeller 선언

---
+++ object/src/main/java/ticket/TicketSeller.java
@@ -0,0 +1,14 @@
+package ticket;
+
+public final class TicketSeller {
+
+  private final TicketOffice ticketOffice;
+
+  public TicketSeller(TicketOffice ticketOffice) {
+    this.ticketOffice = ticketOffice;
+  }
+
+  public TicketOffice getTicketOffice() {
+    return ticketOffice;
+  }
+}
