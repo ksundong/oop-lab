34cfb0297e66809adf29268426470eaa8842523d
Dion <ksundong@gmail.com>
2021-05-17 23:00:50 +0900

    feat: 사용자들이 예매하는 대상인 상영을 구현

---
+++ object/src/main/java/movie/Screening.java
@@ -0,0 +1,28 @@
+package movie;
+
+import java.time.LocalDateTime;
+
+public final class Screening {
+
+  private final Movie movie;
+  private final int sequence;
+  private final LocalDateTime whenScreened;
+
+  public Screening(Movie movie, int sequence, LocalDateTime whenScreened) {
+    this.movie = movie;
+    this.sequence = sequence;
+    this.whenScreened = whenScreened;
+  }
+
+  public Movie getMovie() {
+    return movie;
+  }
+
+  public int getSequence() {
+    return sequence;
+  }
+
+  public LocalDateTime getWhenScreened() {
+    return whenScreened;
+  }
+}
