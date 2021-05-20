060af7f4d3e10da34fbdeb73424c411843aaca3a
Dion <ksundong@gmail.com>
2021-05-20 21:56:05 +0900

    feat: 예약 정보를 담당하는 Reservation 작성

--- object/src/main/java/movie/Reservation.java
+++ object/src/main/java/movie/Reservation.java
@@ -2,5 +2,15 @@ package movie;
 
 public class Reservation {
 
-  public Reservation(Customer customer, Screening screening, Money money, int audienceCount) {}
+  private final Customer customer;
+  private final Screening screening;
+  private final Money money;
+  private final int audienceCount;
+
+  public Reservation(Customer customer, Screening screening, Money money, int audienceCount) {
+    this.customer = customer;
+    this.screening = screening;
+    this.money = money;
+    this.audienceCount = audienceCount;
+  }
 }
