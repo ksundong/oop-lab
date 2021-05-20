f2f31a00f234415995ddfa23d3191bb5dd31b46c
Dion <ksundong@gmail.com>
2021-05-20 23:22:52 +0900

    feat: 일정 금액 할인 정책 구현

---
+++ object/src/main/java/movie/discountpolicy/AmountDiscountPolicy.java
@@ -0,0 +1,20 @@
+package movie.discountpolicy;
+
+import movie.Money;
+import movie.Screening;
+import movie.discountcondition.DiscountCondition;
+
+public class AmountDiscountPolicy extends DiscountPolicy {
+
+  private final Money discountAmount;
+
+  public AmountDiscountPolicy(Money discountAmount, DiscountCondition... conditions) {
+    super(conditions);
+    this.discountAmount = discountAmount;
+  }
+
+  @Override
+  protected Money getDiscountAmount(Screening screening) {
+    return discountAmount;
+  }
+}
