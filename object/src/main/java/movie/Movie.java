54761629a4cab2370a508e36bba51f90a5127bdf
Dion <ksundong@gmail.com>
2021-05-23 23:39:40 +0900

    feat: 실행시점에 할인정책을 간단하게 바꿀 수 있는 메서드 추가

--- object/src/main/java/movie/Movie.java
+++ object/src/main/java/movie/Movie.java
@@ -8,7 +8,7 @@ public class Movie {
   private final String title;
   private final Duration runningTime;
   private final Money fee;
-  private final DiscountPolicy discountPolicy;
+  private DiscountPolicy discountPolicy;
 
   public Movie(String title, Duration runningTime, Money fee, DiscountPolicy discountPolicy) {
     this.title = title;
@@ -23,5 +23,9 @@ public class Movie {
 
   public Money calculateMovieFee(Screening screening) {
     return fee.minus(discountPolicy.calculateDiscountAmount(screening));
+  }
+
+  public void changeDiscountPolicy(DiscountPolicy discountPolicy) {
+    this.discountPolicy = discountPolicy;
   }
 }
