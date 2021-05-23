53fb05619deb6a8664ec54f87bd0061e8177046a
Dion <ksundong@gmail.com>
2021-05-24 00:16:05 +0900

    test: 할인되지 않는 영화 생성 테스트 작성

---
+++ object/src/test/java/movie/MovieIntegrationTest.java
@@ -0,0 +1,27 @@
+package movie;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Duration;
+import movie.discountpolicy.NoneDiscountPolicy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MovieIntegrationTest {
+
+  @Test
+  @DisplayName("할인 되지 않는 영화 생성")
+  void createNoneDiscountMovie() {
+    // given
+    Money fee = Money.wons(10000);
+
+    // when
+    Movie starWars = new Movie("스타워즈",
+        Duration.ofMinutes(210),
+        fee,
+        new NoneDiscountPolicy());
+
+    // then
+    assertThat(starWars.getFee()).isEqualTo(fee);
+  }
+}
