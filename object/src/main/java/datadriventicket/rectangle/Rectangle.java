8d47d64c4b5484c2123b733cebec6cf93f1462b3
Dion <ksundong@gmail.com>
2021-05-27 00:19:07 +0900

    refactor: 자신의 크기를 스스로 증가시키는 책임을 가지는 코드로 변경

--- object/src/main/java/datadriventicket/rectangle/Rectangle.java
+++ object/src/main/java/datadriventicket/rectangle/Rectangle.java
@@ -14,35 +14,8 @@ public class Rectangle {
     this.bottom = bottom;
   }
 
-  public int getLeft() {
-    return left;
-  }
-
-  public void setLeft(int left) {
-    this.left = left;
-  }
-
-  public int getTop() {
-    return top;
-  }
-
-  public void setTop(int top) {
-    this.top = top;
-  }
-
-  public int getRight() {
-    return right;
-  }
-
-  public void setRight(int right) {
-    this.right = right;
-  }
-
-  public int getBottom() {
-    return bottom;
-  }
-
-  public void setBottom(int bottom) {
-    this.bottom = bottom;
+  public void enlarge(int multiple) {
+    right *= multiple;
+    bottom *= multiple;
   }
 }
